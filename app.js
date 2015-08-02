ec71b6c147251d6141151bee566e6651c2e9813f
YF <jackyangfan@corp.netease.com>
2015-08-02 10:56:19 +0800

    misc:爬虫

--- app.js
+++ app.js
@@ -16,7 +16,41 @@ superagent.get(targetUrl)
         $('#topic_list .topic_title').each(function(idx, element){
         	var $element = $(element);
         	var href = url.resolve(targetUrl, $element.attr('href'));
-        	console.log(href);
+        	//console.log(href);
+        	topicUrls.push(href);
+        });
+        
+        var ep = new eventproxy();
+        //params: eventname(String) 事件名,times(Number) 监听次数, callback 回调函数
+        ep.after('topic_html',topicUrls.length,function(topics){
+        	topics = topics.map(function(topicPair){
+        		topicPair = topicPair || [];
+        		var topicUrl = topicPair[0] || '';
+        		var topicHtml = topicPair[1] || '';
+        		var $ = cheerio.load(topicHtml);
+        		return ({
+        			title:$('.topic_full_title').text().trim(),
+        		    href:topicUrl,
+        		    comment1:$('.reply_content').eq(0).text().trim()
+        		});
+        		
+        	});
+        	
+        	console.log('outcome:');
+        	console.log(topics);
+        });
+        
+        topicUrls.forEach(function(topicUrl){
+        	superagent.get(topicUrl)
+        	    .end(function(err, res){
+        	    	if(err){
+        	    		console.error('fetch ' + topicUrl + ' fail');
+        	    		ep.emit('topic_html',[topicUrl,''])
+        	    		return;
+        	    	}
+        	    	console.error('fetch ' + topicUrl + ' success');
+        	    	ep.emit('topic_html',[topicUrl,res.text]);
+        	    });
         });
         
     });
