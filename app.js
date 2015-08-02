16ce07aeecb933a9c99155ba9fd3146065dc6343
YF <jackyangfan@corp.netease.com>
2015-08-02 10:40:51 +0800

    misc:爬虫

---
+++ app.js
@@ -0,0 +1,23 @@
+var url = require('url');
+var express = require('express');
+var cheerio = require('cheerio');
+var superagent = require('superagent');
+var eventproxy = require('eventproxy');
+
+var targetUrl = 'https://cnodejs.org/';
+superagent.get(targetUrl)
+    .end(function (err, res) {
+        if(err){
+        	return console.error(err);
+        }
+        var topicUrls = [];
+        var $ = cheerio.load(res.text);
+        //获取首页所有链接
+        $('#topic_list .topic_title').each(function(idx, element){
+        	var $element = $(element);
+        	var href = url.resolve(targetUrl, $element.attr('href'));
+        	 console.log(href);
+        	
+        });
+        
+    });
