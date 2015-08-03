50609e3630b4e96db2aa6ae386c7eb791299b5c1
YF <jackyangfan@corp.netease.com>
2015-08-03 23:51:31 +0800

    misc:抓验证码

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var url = require('url');
 var http = require('http');
+var cookie = require('cookie');
 //var request = require('request');
 var express = require('express');
 var cheerio = require('cheerio');
@@ -8,20 +9,19 @@ var superagent = require('superagent');
 var eventproxy = require('eventproxy');
 var iconv = require('iconv-lite');
 var BufferHelper = require('bufferhelper');
+var cookieparser = require('cookie-parser');
 
 var targetUrl = 'http://gsxt.zjaic.gov.cn/search/doGetAppSearchResultIn.do';
 
 var headers = {
+    Accept: 'image/webp,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, sdch',
+    'Accept-Language': 'zh-CN,zh;q=0.8',
+    Cookie:'',
     Host: 'gsxt.zjaic.gov.cn',
-    Accept: 'image/png,image/*;q=0.8,*/*;q=0.5',
-    'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36',
-    'Content-Type': 'application/x-www-form-urlencoded',
+    'Proxy-Connection': 'keep-alive',
     Referer: 'http://gsxt.zjaic.gov.cn/search/doGetAppSearchResultIn.do',
-    cookie:'CNZZDATA1000503299=1618994926-1437749885-%7C1438513840; JSESSIONID=37F63EE7F09C5595CD6062D3FDEEE831-n1.gsxt46',
-    'Accept-Encoding': 'gzip, deflate',
-    Connection: 'keep-alive',
-    'Cache-Control': 'max-age=0'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36'
 };
 
 
@@ -55,14 +55,24 @@ superagent.get(targetUrl).end(function (err, res) {
         }
         
         //console.log(res.text);
-
+        //console.log(res.headers);
+        //console.log(res.headers);
+        //console.log(res.headers['set-cookie']);
+        //console.log(res.headers.cookie);
+        //cookie.parse(req.headers.cookie)
+        var sid = cookie.parse(res.headers['set-cookie'][0])['JSESSIONID'];
+        //console.log(sid);
+        headers.Cookie = 'JSESSIONID=' + sid;
+        //console.log(headers.cookie);
+        //headers.cookie = 'CNZZDATA1000503299=1155596647-1437749885-http%253A%252F%252Fgsxt.zjaic.gov.cn%252F%7C1438531438; JSESSIONID=' + sid;
+        //headers.cookie ='JSESSIONID=26A95651B79D15132485FE9DA2E534D8-n1.gsxt46; CNZZDATA1000503299=1155596647-1437749885-http%253A%252F%252Fgsxt.zjaic.gov.cn%252F%7C1438609956';
         var $ = cheerio.load(res.text);
         $('#kaptchaImg').each(function(idx, element){
         	//console.log(element);
         	//var imgsrc = url.resolve(targetUrl, element.attribs.src);
         	var imgsrc = url.resolve(targetUrl, $(element).attr('src'));
-        	imgsrc = 'http://imglf.nosdn.127.net/img/TGJDYUZPNmdKdTlQdmd5NjArZ1VVZVlKejhsUksrSGo.jpg?imageView&thumbnail=500x0&quality=96&stripmeta=0&type=jpg';
-        	console.log(imgsrc);
+        	//imgsrc = 'http://imglf.nosdn.127.net/img/TGJDYUZPNmdKdTlQdmd5NjArZ1VVZVlKejhsUksrSGo.jpg?imageView&thumbnail=500x0&quality=96&stripmeta=0&type=jpg';
+        	//console.log(imgsrc);
         	var writestream = fs.createWriteStream('aaaaa.jpg');
         	
         	//.set(headers)
@@ -79,18 +89,22 @@ superagent.get(targetUrl).end(function (err, res) {
         		res.pipe(writestream);
         	});
         	*/
-	        var options = {
+	
+        	var options = {
 	        	host: 'gsxt.zjaic.gov.cn',
 	            hostname: 'gsxt.zjaic.gov.cn',
 	            port: 80,
 	            method: 'get',
 	            path: '/common/captcha/doReadKaptcha.do',
-	            headers:{}
+	            headers:headers
 	        };
+        	
+	        console.log(headers.Cookie);
+	        console.log(options.headers);
 	        //options.headers = headers;
-	        options.host = 'imglf.nosdn.127.net';
-	        options.hostname = 'imglf.nosdn.127.net';
-	        options.path = '/img/TGJDYUZPNmdKdTlQdmd5NjArZ1VVZVlKejhsUksrSGo.jpg?imageView&thumbnail=500x0&quality=96&stripmeta=0&type=jpg';
+	       ////options.host = 'imglf.nosdn.127.net';
+	       ////options.hostname = 'imglf.nosdn.127.net';
+	        ////options.path = '/img/TGJDYUZPNmdKdTlQdmd5NjArZ1VVZVlKejhsUksrSGo.jpg?imageView&thumbnail=500x0&quality=96&stripmeta=0&type=jpg';
 	        
 	        var req = http.request(options,function(res){
 	        	console.log('111');
