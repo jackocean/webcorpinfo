f6c6a0d8fc33d670f2957d3ab508f0ab366f950a
YF <jackyangfan@corp.netease.com>
2015-08-08 21:49:47 +0800

    misc:爬虫

--- app.js
+++ app.js
@@ -12,8 +12,10 @@ var cookieparser = require('cookie-parser');
 
 var targetUrl = 'http://gsxt.zjaic.gov.cn/search/doGetAppSearchResultIn.do';
 
-var headers = {
-	Accept : 'image/webp,*/*;q=0.8',
+
+
+var headers2 = {
+	Accept : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
 	/*
 	 * 'Accept-Encoding': 'gzip, deflate, sdch',
 	 * 傻逼的网站对图片进行了gzip，浏览器可以ungzip，但这个nodejs发的请求不能ungzip
@@ -21,11 +23,11 @@ var headers = {
 	'Accept-Language' : 'zh-CN,zh;q=0.8',
 	Cookie : '',
 	Host : 'gsxt.zjaic.gov.cn',
-	'Proxy-Connection' : 'keep-alive',
-	Referer : 'http://gsxt.zjaic.gov.cn/search/doGetAppSearchResultIn.do',
+	'Connection' : 'keep-alive',
 	'User-Agent' : 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36'
 };
 
+/*
 var getValcode = function(cb) {
 	superagent.get(targetUrl).end(function(err, res) {
 		if (err) {
@@ -85,42 +87,15 @@ var getValcode = function(cb) {
 
 	});
 };
-
-// getValcode(function(){});
+*/
 
 http.createServer(function(request, response) {
-
-	// response.write("Hello World");
-	// getValcode((function(response) {
-	// var res = response;
-	// return function(img){
-	// //res.write("yyyyyy");
-	// res.write(img);
-	// res.end();
-	// };
-	// })(response));
-
-	/*
-	getValcode(function(img) {
-		// response.writeHead(200, {
-		// "Accept-Ranges": "bytes",
-		// "Content-Type" : "image/jpeg",
-		// "Content-Length": img.length
-		// });
-		response.writeHead(200, {
-			"Content-Type" : "image/jpeg"
-		});
-		response.write(img,'binary');
-		response.end();
-	});
-	*/
-	
-	
 	var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
     
     var sessionid = '';
     if(pathname=='/valcode'){
+        /*
     	getValcode(function(img) {
     		// response.writeHead(200, {
     		// "Accept-Ranges": "bytes",
@@ -133,6 +108,73 @@ http.createServer(function(request, response) {
     		response.write(img,'binary');
     		response.end();
     	});
+    	*/
+    	var headers = {
+	        Accept : 'image/webp,*/*;q=0.8',
+	        /*
+	         * 'Accept-Encoding': 'gzip, deflate, sdch',
+	         * 傻逼的网站对图片进行了gzip，浏览器可以ungzip，但这个nodejs发的请求不能ungzip
+	         */
+	        'Accept-Language' : 'zh-CN,zh;q=0.8',
+	        Cookie : '',
+	        Host : 'gsxt.zjaic.gov.cn',
+	        'Connection' : 'keep-alive',
+	        Referer : 'http://gsxt.zjaic.gov.cn/search/doGetAppSearchResultIn.do',
+	        'User-Agent' : 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36'
+        };
+    	
+    	var options = {
+			host : 'gsxt.zjaic.gov.cn',
+			hostname : 'gsxt.zjaic.gov.cn',
+			port : 80,
+			method : 'get',
+			path : '/common/captcha/doReadKaptcha.do',
+			headers : headers
+		};
+    	
+		var getValcode = function(){
+			http.get(options, function(res) {
+				var imgdata = '';
+				res.setEncoding('binary');
+
+				res.on('data', function(chunk) {
+					imgdata += chunk;
+				});
+
+				res.on('end', function() {
+					response.writeHead(200, {
+		    			"Content-Type" : "image/jpeg"
+		    		});
+		    		response.write(imgdata,'binary');
+		    		response.end();
+				});
+			});
+		};
+    	
+		var options2 = {
+			host : 'gsxt.zjaic.gov.cn',
+			hostname : 'gsxt.zjaic.gov.cn',
+			port : 80,
+			method : 'head',
+			path : '/search/doGetAppSearchResultIn.do',
+			headers : headers2
+		};
+    	
+		if(!!sessionid){
+			headers.Cookie = 'JSESSIONID=' + sessionid;
+			getValcode();
+		} else{
+			http.get(options2, function(res) {
+				//console.log(res);
+				var sid = cookie.parse(res.headers['set-cookie'][0])['JSESSIONID'];
+				sessionid = sid;
+				headers.Cookie = 'JSESSIONID=' + sessionid;
+				//console.log(sid);//每次sessionid都不同
+				
+				getValcode();
+			});
+		}
+		
         return;
     }
     
@@ -152,3 +194,4 @@ http.createServer(function(request, response) {
 
 
 }).listen(8888);
+
